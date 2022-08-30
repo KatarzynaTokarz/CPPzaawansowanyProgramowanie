43ff7394daa1d5966d0e396d58a25f7bec91484b
Uzytkownik <uzytkownik@kursGIT.pl>
2022-08-30 18:56:24 +0200

    zad2

---
+++ Zad3Zaawansowane/Zad3Zaawansowane.cpp
@@ -0,0 +1,17 @@
+//Zadanie 3
+//Napisz klasê StringConverter, która bedzie mia³a dwie metody(statyczne) :
+//	std::string toCamelCase(std::string)
+//	std::string toSnakeCase(std::string)
+//	camelCase : snake_case
+//	helloSDA : hello_S_D_A
+//	getColour : get_colour
+//	isThisFirstEntry : is_this_first_entry
+
+#include <iostream>
+
+class
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
